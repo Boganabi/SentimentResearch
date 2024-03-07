1ab59af110a36066f505b23888aadf78eeeff0d8
Boganabi <logan.ashbaugh0001@gmail.com>
2024-03-06 20:24:22 -0800

    added eval code file

---
+++ evaluateAllModels.py
@@ -0,0 +1,7 @@
+
+# what i want to do is have the output csv have these columns:
+# actual rating, rating text, RNN value, CNN value, LR value, NB value, RF value, NLTK value, TextBlob value
+
+import pandas as pd
+
+df = pd.read_csv("./balanced_data.csv")
